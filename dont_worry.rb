d5a21d2afa611a61037699e16d078e6a3cd0c055
thecarsgone <carolina.nicole.gonzalez@gmail.com>
2016-08-01 15:47:21 -0400

    Done

--- dont_worry.rb
+++ dont_worry.rb
@@ -10,7 +10,17 @@ def with_block(uri)
   end
 end
 
-# with_block(uri)
+# explanation
+Net::HTTP.start #the HTTP.start method opens up a TCP/IP connection, just like a client does.
+
+(uri.host, uri.port) #to do so, it calls on that host and port information
+
+ do |http| #the block format is to keep using that same connection session for this bit of code
+
+  my_get_request = Net::HTTP::Get.new uri # we're defining a GET request from that location
+
+  server_response = http.request request # We're now making that specific request, and storing it in an HTTP response object
+end
 
 
 def no_block(uri)
